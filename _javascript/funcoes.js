96746b36fbcefa638d426c18b2f9f28f799bb4b8
Joaocode <joao.manoeek563@gmail.com>
2023-09-18 19:59:51 -0300

    Sobre googleglass

---
+++ _javascript/funcoes.js
@@ -0,0 +1,40 @@
+function fotocasa(foto) {
+    var image = document.getElementById("icone");
+    image.src = "imagens/home.png"
+}
+function tirafotocasa(foto) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/glass-oculos-preto-peq.png"
+}
+function especificacoes(foto) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/especificacoes.png"
+}
+function tiraespecificacoes(foto) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/glass-oculos-preto-peq.png"
+}
+function camera(foto) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/fotos.png"
+}
+function tiracamera(fotos) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/glass-oculos-preto-peq.png"
+}
+function multimidia(fotos) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/multimidia.png"
+}
+function tiramultimidia(fotos) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/glass-oculos-preto-peq.png"
+}
+function contato(fotos) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/contato.png"
+}
+function tiracontato(fotos) {
+    var image = document.getElementById("icone")
+    image.src = "imagens/glass-oculos-preto-peq.png"
+}
